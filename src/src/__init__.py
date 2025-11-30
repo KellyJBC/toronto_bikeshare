534b72c7a1cb243b508e49775b8aa6b75eb06b88
KellyJBC <kellyjbolanos@gmail.com>
2025-11-29 23:35:49 -0500

    Create __init__.py

---
+++ src/src/__init__.py
@@ -0,0 +1,9 @@
+"""
+Toronto Bike-Sharing Analytics Tool
+
+This package contains modules for:
+- Loading and cleaning data
+- Computing analytics summaries
+- Generating plots
+- Running a Streamlit dashboard
+"""
