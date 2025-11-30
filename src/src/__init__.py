ff789100b139ab31023ffb34d8c37308c85995fe
KellyJBC <kellyjbolanos@gmail.com>
2025-11-29 23:36:04 -0500

    Delete src/src directory

--- src/src/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Toronto Bike-Sharing Analytics Tool
-
-This package contains modules for:
-- Loading and cleaning data
-- Computing analytics summaries
-- Generating plots
-- Running a Streamlit dashboard
-"""
